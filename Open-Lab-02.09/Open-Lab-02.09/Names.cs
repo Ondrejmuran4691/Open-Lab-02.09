aab5fca4ad1ba42b6709f3c537ef6c20a429de7c
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-10-09 18:59:46 +0200

    done

---
+++ Open-Lab-02.09/Open-Lab-02.09/Names.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._09
+{
+    public class Names
+    {
+        public string ConcatName(string firstName, string lastName)
+        {
+            string people = (lastName + ", " + firstName);
+            return people;
+        }
+    }
+}
